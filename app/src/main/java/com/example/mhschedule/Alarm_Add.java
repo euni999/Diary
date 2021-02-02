1a0ac08742287f45e5cf800ff401a5dda72b99e2
boo105 <boo10500@naver.com>
2021-02-02 19:47:10 +0900

    알람 반복 구현

--- app/src/main/java/com/example/mhschedule/Alarm_Add.java
+++ app/src/main/java/com/example/mhschedule/Alarm_Add.java
@@ -14,9 +14,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 public class Alarm_Add extends AppCompatActivity {
+    static final String TAG = "Alarm_Add";
+
     AlarmManager alarmManager;
     TimePicker timePicker;
     Context context;
@@ -31,55 +39,80 @@ public class Alarm_Add extends AppCompatActivity {
 
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         timePicker = findViewById(R.id.time_picker);
+    }
 
+    public void setAlarm(View view)
+    {
+        MaterialButtonToggleGroup buttonToggleGroup = findViewById(R.id.toggle_button_group);
         final Calendar calendar = Calendar.getInstance();
 
-        // 알람 시작? 버튼
-        Button button = (Button) findViewById(R.id.set_alarm);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 시간 가져오기
-                int hour = timePicker.getHour();
-                int minute = timePicker.getMinute();
+        // 요일 값 초기화
+        boolean[] weekends = {false,false,false,false,false,false,false};
+        Log.i(TAG,buttonToggleGroup.getCheckedButtonIds().toString());
+        int[] ButtonsIds = buttonToggleGroup.getCheckedButtonIds().stream().mapToInt(i->i).toArray();
+        for(int number : ButtonsIds)
+            weekends[number-1] = true;
 
-                // calendar에 시간 세팅
-                calendar.set(Calendar.HOUR_OF_DAY,hour);
-                calendar.set(Calendar.MINUTE,minute);
-                calendar.set(Calendar.SECOND,0);
-                Toast.makeText(Alarm_Add.this,"알람 예정 " + hour + "시 " + minute + "분",Toast.LENGTH_SHORT).show();
+        // 시간 가져오기
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
 
-                Log.i("STATE","예약 시간 : " + calendar.getTime().toString());
+        // calendar에 시간 세팅
+        calendar.set(Calendar.HOUR_OF_DAY,hour);
+        calendar.set(Calendar.MINUTE,minute);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        Toast.makeText(Alarm_Add.this,"알람 예정 " + hour + "시 " + minute + "분",Toast.LENGTH_SHORT).show();
 
-                // 알람 리시버 intent 생성
-                final Intent alarm_intent = new Intent(context, Alarm_Receiver.class);
-                // 액션 설정
-                alarm_intent.setAction(Alarm_Receiver.ACTION_RESTART_SERVICE);
+        Log.i("STATE","예약 시간 : " + calendar.getTime().toString());
 
-                // receiver에 string 값 넘겨주기
-               // alarm_intent.putExtra("state","alarm on");
+        // 알람 리시버 intent 생성
+        final Intent alarm_intent = new Intent(context, Alarm_Receiver.class);
+        // 액션 설정
+        alarm_intent.setAction(Alarm_Receiver.ACTION_RESTART_SERVICE);
+        // 선택한 요일 값들 넘겨줌
+        alarm_intent.putExtra("weekend",weekends);
 
-                // pendingIntent 설정
-                pendingIntent = PendingIntent.getBroadcast(Alarm_Add.this,0,alarm_intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        // pendingIntent 설정
+        pendingIntent = PendingIntent.getBroadcast(Alarm_Add.this,0,alarm_intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
+        // 하루의 시간을 나타냄
+        long intervalDay = 1000 * 60 * 60  * 24;
 
-                /*
-                sdk 23 이상부터는 Doze 모드가 있어 디바이스가 잠들면 알람매니저가 안먹힘
+        long selectTime = calendar.getTimeInMillis();
+        long currentTime = System.currentTimeMillis();
 
-                https://developer.android.com/training/monitoring-device-state/doze-standby
-                참조
-                * */
+        //만일 내가 설정한 시간이 현재 시간보다 작다면 알람이 바로 울려버리기 때문에 이미 시간이 지난 알람은 다음날 울려야 한다.
+        if(currentTime>selectTime)
+            selectTime += intervalDay;
 
-                // 알람설정
-                // set 함수보다 setExact 함수가 더 정확하게 스케쥴링 됨
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        /*
+        sdk 23 이상부터는 Doze 모드가 있어 디바이스가 잠들면 알람매니저가 안먹힘
 
-                //alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
-                Log.i("STATE","알람 설정 성공!");
-            }
-        });
+        https://developer.android.com/training/monitoring-device-state/doze-standby
+        참조
+        */
+
+        // 알람설정
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,selectTime,intervalDay,pendingIntent);
+
+        /*
+        // 한번만 실행됨
+        // set 함수보다 setExact 함수가 더 정확하게 스케쥴링 됨
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);*/
+
+        //alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+        Log.i("STATE","알람 설정 성공!");
     }
+
+    public void cancelAlarm(View view)
+    {
+        Intent intent = new Intent(this, Alarm.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        alarmManager.cancel(pendingIntent);
+    }
+
 }
